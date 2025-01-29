00a11fb5dc98416fc2f1e7e7039084e4a0f183da
Daniel <daniel.corbellini28@gmail.com>
2025-01-29 18:38:50 -0300

    feat(#208): Link between routes and make the PageNav component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-    return (
-        <div>
-            test Homepage
-        </div>
-    )
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
