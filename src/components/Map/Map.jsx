ed6af870c3968c668e578dc7a9c146fc27d20532
Daniel <daniel.corbellini28@gmail.com>
2025-02-09 15:05:44 -0300

    feat(#234): Fetch city data in the form

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -12,20 +12,18 @@ import { useEffect, useState } from "react";
 import { useCities } from "../../context/CitiesContext/CitiesContext";
 import { useGeolocation } from "../../hooks/useGeolocation/useGeolocation";
 import Button from "../Button/Button";
+import { useUrlPosition } from "../../hooks/useUrlPosition/useUrlPosition";
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geoLocationPosition,
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
-
+  const [mapLat, mapLng] = useUrlPosition();
   useEffect(() => {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
