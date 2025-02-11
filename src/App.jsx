d351cbf91b0c3b44e2a7c236f617b7442cdcaf7d
Daniel <daniel.corbellini28@gmail.com>
2025-02-11 19:22:45 -0300

    feat(#240): Adds protect route

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import City from "./components/City/City";
 import Form from "./components/Form/Form";
 import { CitiesProvider } from "./context/CitiesContext/CitiesContext";
 import { AuthProvider } from "./context/FakeAuthContext/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
@@ -25,7 +26,14 @@ function App() {
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
 
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
