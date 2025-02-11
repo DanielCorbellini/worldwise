6527d7c6e6e912b5e1c18171c2ff4d1182c58a7e
Daniel <daniel.corbellini28@gmail.com>
2025-02-11 18:29:58 -0300

    feat(#239): Adds fake authentication

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,16 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/FakeAuthContext/FakeAuthContext";
 import styles from "./User.module.css";
 
-const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
-};
-
 function User() {
-  const user = FAKE_USER;
+  const navigate = useNavigate();
 
-  function handleClick() {}
+  const { user, logout } = useAuth();
+
+  function handleClick(e) {
+    e.preventDefault();
+    logout();
+    navigate("/");
+  }
 
   return (
     <div className={styles.user}>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../../components/PageNav/PageNav";
 import styles from "./Login.module.css";
+import { useAuth } from "../../context/FakeAuthContext/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const { isAuthenticated, login } = useAuth();
+
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState("jack@test.com");
+  const [password, setPassword] = useState("12345");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    login(email, password);
+  }
+
+  useEffect(() => {
+    if (isAuthenticated) navigate("/app");
+  }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +47,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
